cb2f6feced10b1d5d5b1324ad412db1a64bfc227
Efrizal Nurul Fajri <admin@masnurulhuda.sch.id>
2021-04-07 12:26:04 +0700

    Added DAO

---
+++ app/src/main/java/id/zlz/mapsdemo/db/BookmarkDao.kt
@@ -0,0 +1,31 @@
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import androidx.room.parser.QueryType
+import com.google.common.net.HttpHeaders.FROM
+import id.zlz.mapsdemo.model.Bookmark
+
+@Dao
+interface BookmarkDao {
+    @Query("SELECT * FROM Bookmark")
+    fun loadAll(): LiveData<List<Bookmark.Bookmark>>
+
+    @Query("SELECT * FROM Bookmark WHERE id = :bookmarkId")
+    fun loadBookmark(bookmarkId: Long) : Bookmark
+
+    @Query("SELECT * FROM Bookmark WHERE id = :bookmarkId")
+    fun loadLiveBookmark(bookmarkId: Long): LiveData<Bookmark>
+
+    @Insert(onConflict = IGNORE)
+    fun insertBookmark(bookmark: Bookmark): Long
+    
+    @Insert(onConflict = REPLACE)
+    fun updateBookmark(bookmark: Bookmark)
+
+    @Delete
+    fun deleteBookmark(delete: Bookmark)
+}
---
+++ app/src/main/java/id/zlz/mapsdemo/model/Bookmark.kt
@@ -0,0 +1,21 @@
+package id.zlz.mapsdemo.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class Bookmark {
+
+    @Entity
+    data class Bookmark(
+        @PrimaryKey(autoGenerate = true)
+        var id: Long? = null,
+        var placeid: String? = "",
+        var name: String? = "",
+        var address: String? = "",
+        var lattitude: Double? = 0.0,
+        var longitude: Double? = 0.0,
+        var phone:String = ""
+    )
+
+
+}
